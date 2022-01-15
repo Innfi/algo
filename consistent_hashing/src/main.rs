f6c3d169fd1f86f1da9a8bbb98743347caff88aa
innfi <innfi.world@gmail.com>
2022-01-15 23:27:51 +0900

    consistent-hash: researching baseline data structure

--- consistent_hashing/src/main.rs
+++ consistent_hashing/src/main.rs
@@ -1,7 +1,7 @@
 /**
 TODO
 -------------------------------------------------------------------------------
-ring buffer inside integer value
+ring buffer 
 assign a node (cache server, specifically) to buffer
 assign a key to node
 assign a hash value to node 
@@ -12,6 +12,25 @@ apply virtual node to buffer
 DONE
 -------------------------------------------------------------------------------
  */
+
+pub struct NodeKey {
+    value: String,
+}
+
+pub struct NodeServer {
+    url: String,
+}
+
+pub struct Controller {
+    buffer: Vec<String>,
+    nodeCount: u32,
+}
+
+impl Controller {
+    pub fn addServer(&mut self, server: NodeServer) {
+        self.buffer.push(server.url);
+    }
+}
 
 fn main() {
     println!("Hello, world!");
