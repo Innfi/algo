28ca24a6df5ebe9dfcd706daa3e9b2b08816d133
innfi <innfi.world@gmail.com>
2022-02-16 08:18:56 +0900

    consistent_hashing: add_node

--- consistent_hashing/src/innfis_hash.rs
+++ consistent_hashing/src/innfis_hash.rs
@@ -7,7 +7,8 @@ pub struct TinyNode {
 }
 
 pub struct TinyHashRing {
-  pub rings: Vec<TinyNode>,
+  rings: Vec<TinyNode>,
+  virtual_node_len: usize,
 }
 
 fn to_initial_nodes(
@@ -47,9 +48,56 @@ fn to_virtual_node_id(node: &TinyNode, index: usize) -> String {
 }
 
 impl TinyHashRing {
+  // new
   pub fn new(nodes: &Vec<TinyNode>, virtual_node_len: usize) -> Self {
-    let initial_nodes = to_initial_nodes(nodes, virtual_node_len);
+    let mut initial_nodes = to_initial_nodes(nodes, virtual_node_len);
+    initial_nodes.sort_by(|a, b| a.hash.cmp(&b.hash));
 
-    Self { rings: initial_nodes }
+    Self { rings: initial_nodes, virtual_node_len }
+  }
+
+  // rings_view
+  pub fn rings_view(&self) -> &Vec<TinyNode> {
+    &self.rings
+  }
+
+  // add_node
+  pub fn add_node(&mut self, new_node: &TinyNode) {
+    let mut hasher = Sha1::new();
+
+    let mut new_nodes: Vec<TinyNode> = Vec::new();
+
+    for n in 0..self.virtual_node_len {
+      let node_id: String = to_virtual_node_id(new_node, n);
+      hasher.input_str(node_id.as_str());
+     
+      let virtual_node = TinyNode {
+        url: new_node.url.clone(),
+        hash: hasher.result_str(),
+      };
+
+      hasher.reset();
+
+      new_nodes.push(virtual_node);
+    }
+
+    self.add_nodes(new_nodes);
+  }
+
+  fn add_nodes(&mut self, new_nodes: Vec<TinyNode>) {
+    for new_node in new_nodes {
+      let search_result = self.rings.binary_search_by(|x| {
+        x.hash.cmp(&new_node.hash)
+      });
+
+      let index: usize = search_result.unwrap_or_else(|x| x);
+
+      self.rings.insert(index, new_node);
+    }
+  }
+
+  // remove_node
+  pub fn remove_node(&mut self, target_node: &TinyNode) {
+    // TODO
   }
 }
--- consistent_hashing/src/lib.rs
+++ consistent_hashing/src/lib.rs
@@ -1,8 +1,10 @@
 mod innfis_hash;
+use innfis_hash::TinyNode;
+use innfis_hash::TinyHashRing;
 
 #[test]
 fn node_has_basic_fields() {
-  let node = innfis_hash::TinyNode {
+  let node = TinyNode {
       url: String::from("test_url"),
       hash: String::from(""),
   };
@@ -11,57 +13,116 @@ fn node_has_basic_fields() {
 }
 
 #[test]
-fn consistent_hash_has_empty_ring() {
-  let ring_instance = innfis_hash::TinyHashRing {
-    rings: vec![]
-  };
-
-  assert_eq!(ring_instance.rings.is_empty(), true);
-}
-
-#[test]
 fn access_consistent_hash_with_impl() {
-  let node = innfis_hash::TinyNode {
+  let node = TinyNode {
     url: String::from("test_url"),
     hash: String::from(""),
   };
 
-  let ring_instance = innfis_hash::TinyHashRing::new(&vec![node], 1);
+  let ring_instance = TinyHashRing::new(&vec![node], 1);
 
-  assert_eq!(ring_instance.rings.len(), 1);
+  let view = ring_instance.rings_view();
+  assert_eq!(view.len(), 1);
 }
 
 #[test]
 fn input_nodes_get_hashes() {
-  let node = innfis_hash::TinyNode {
+  let node = TinyNode {
     url: String::from("test_url"),
     hash: String::from(""),
   };
 
-  let ring_instance = innfis_hash::TinyHashRing::new(&vec![node], 1);
+  let ring_instance = TinyHashRing::new(&vec![node], 1);
 
-  assert_ne!(ring_instance.rings[0].hash, String::from(""));
+  let view = ring_instance.rings_view();
+  assert_ne!(view[0].hash, String::from(""));
 }
 
 #[test]
 fn consistent_hash_has_virtual_node_len() {
-  let node = innfis_hash::TinyNode {
-    url: String::from("test_url"),
-    hash: String::from(""),
-  };
-
+  let initial_nodes: Vec<TinyNode> = vec![
+    TinyNode {
+      url: String::from("http://test-server1:9200"),
+      hash: String::from(""),
+    },
+    TinyNode {
+      url: String::from("http://test-server2:9200"),
+      hash: String::from(""),
+    },
+  ];
   let virtual_node_len: usize = 5;
-  let ring_instance = innfis_hash::TinyHashRing::new(
-    &vec![node],
+  let expected_node_len: usize = 10;
+  
+  let ring_instance = TinyHashRing::new(
+    &initial_nodes,
     virtual_node_len
   );
 
-  assert_eq!(ring_instance.rings.len(), virtual_node_len);
-  ring_instance.rings.into_iter().for_each(|x| {
-    println!("url: {}, hash: {}", x.url, x.hash);
+  let view = ring_instance.rings_view();
+  assert_eq!(view.len(), expected_node_len);
+}
+
+#[test]
+fn node_is_sorted_by_hash() {
+  let initial_nodes: Vec<TinyNode> = vec![
+    TinyNode {
+      url: String::from("http://test-server1:9200"),
+      hash: String::from(""),
+    },
+    TinyNode {
+      url: String::from("http://test-server2:9200"),
+      hash: String::from(""),
+    },
+  ];
+  let virtual_node_len: usize = 3;
+
+  let ring_instance = TinyHashRing::new(
+    &initial_nodes,
+    virtual_node_len
+  );
+
+  let mut current_hash: String = String::new();
+
+  let view = ring_instance.rings_view();
+  view.into_iter().for_each(|x| {
+    if !current_hash.is_empty() {
+      assert_eq!(x.hash > current_hash, true);
+    }
+
+    current_hash = x.hash.clone();
   });
 }
 
+#[test]
+fn add_node() {
+  let initial_nodes: Vec<TinyNode> = vec![
+    TinyNode {
+      url: String::from("http://test-server1:9200"),
+      hash: String::from(""),
+    },
+    TinyNode {
+      url: String::from("http://test-server2:9200"),
+      hash: String::from(""),
+    },
+  ];
+  let virtual_node_len: usize = 3;
+  let expected_node_len: usize = 9;
+
+  let mut ring_instance = TinyHashRing::new(
+    &initial_nodes,
+    virtual_node_len
+  );
+
+  let new_node = TinyNode {
+    url: String::from("http://test-server3:9200"),
+    hash: String::from(""),
+  };
+
+  ring_instance.add_node(&new_node);
+
+  let view = ring_instance.rings_view();
+  assert_eq!(view.len(), expected_node_len);
+}
 
 #[cfg(test)]
 mod tests {    
