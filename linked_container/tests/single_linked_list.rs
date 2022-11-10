f0dd50f999ef1a8696c983c08f13031c1972a631
innfi <innfi.world@gmail.com>
2022-11-10 21:13:49 +0900

    linked_container: learned a few with iterators

--- linked_container/tests/single_linked_list.rs
+++ linked_container/tests/single_linked_list.rs
@@ -71,9 +71,9 @@ mod tests_option {
 
 }
 
-// https://rust-unofficial.github.io/too-many-lists/first-final.html
+// https://rust-unofficial.github.io/too-many-lists
 #[cfg(test)]
-mod tests_reference {
+mod tests_reference1 {
   use std::mem;
 
   pub struct List {
@@ -139,5 +139,140 @@ mod tests_reference {
     list.push(5);
     assert_eq!(list.pop().unwrap(), 5);
     assert_eq!(list.pop(), None);
+  }
+}
+
+#[cfg(test)]
+mod tests_reference2 {
+  pub struct List<T> {
+    head: Link<T>,
+  }
+
+  type Link<T> = Option<Box<Node<T>>>;
+
+  struct Node<T> {
+    elem: T,
+    next: Link<T>,
+  }
+
+  impl<T> List<T> {
+    pub fn new() -> Self {
+      List { head: None }
+    }
+
+    pub fn push(&mut self, elem: T) {
+      let new_node = Box::new(Node {
+        elem: elem,
+        next: self.head.take(),
+      });
+
+      self.head = Some(new_node);
+    }
+
+    pub fn pop(&mut self) -> Option<T> {
+      self.head.take().map(|node| {
+        self.head = node.next;
+        node.elem
+      })
+    }
+
+    pub fn into_iter(self) -> IntoIter<T> {
+      IntoIter(self)
+    }
+
+    pub fn iter(&self) -> Iter<'_, T> {
+      Iter { next: self.head.as_deref() }
+    }
+
+    pub fn iter_mut(&mut self) -> IterMut<'_, T> {
+      IterMut { next: self.head.as_deref_mut() }
+    }
+  }
+
+  impl<T> Drop for List<T> {
+    fn drop(&mut self) {
+      let mut cur_link = self.head.take();
+      while let Some(mut boxed_node) = cur_link {
+        cur_link = boxed_node.next.take();
+      }
+    }
+  }
+
+  pub struct IntoIter<T>(List<T>);
+
+  impl<T> Iterator for IntoIter<T> {
+    type Item = T;
+    fn next(&mut self) -> Option<Self::Item> {
+      self.0.pop()
+    }
+  }
+
+  pub struct Iter<'a, T> {
+    next: Option<&'a Node<T>>,    
+  }
+
+  impl<'a, T> Iterator for Iter<'a, T> {
+    type Item = &'a T;
+    fn next(&mut self) -> Option<Self::Item> {
+      self.next.map(|node| {
+        self.next = node.next.as_deref();
+        &node.elem
+      })
+    }
+  }
+
+  pub struct IterMut<'a, T> {
+    next: Option<&'a mut Node<T>>,    
+  }
+
+  impl<'a, T> Iterator for IterMut<'a, T> {
+    type Item = &'a mut T;
+    fn next(&mut self) -> Option<Self::Item> {
+      self.next.take().map(|node| {
+        self.next = node.next.as_deref_mut();
+        &mut node.elem
+      })
+    }
+  }
+
+  #[test]
+  fn test_push_pop() {
+    let mut list = List::new();
+    list.push(String::from("begin"));
+    list.push(String::from("middle"));
+    list.push(String::from("end"));
+
+    assert_eq!(list.pop(), Some(String::from("end")));
+    list.pop();
+    list.pop();
+    assert_eq!(list.pop(), None);
+  }
+
+  #[test]
+  fn test_into_iter() {
+    let mut list = List::new();
+    list.push(String::from("begin"));
+    list.push(String::from("middle"));
+    list.push(String::from("end"));
+
+    let mut iter = list.into_iter();
+    assert_eq!(iter.next(), Some(String::from("end")));
+
+    // illegal as list is moved
+    // assert_eq!(list.pop(), Some(String::from("middle")));
+  }
+
+  #[test]
+  fn test_iter() {
+    let mut list = List::new();
+    list.push(String::from("begin"));
+    list.push(String::from("middle"));
+    list.push(String::from("end"));
+
+    let mut iter = list.iter();
+    assert_eq!(iter.next(), Some(&String::from("end")));
+
+    // list is not live and well
+    assert_eq!(list.pop(), Some(String::from("end")));
   }
 }
