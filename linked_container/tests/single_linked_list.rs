a0cf7b4ad42f36405b1dacc34716ede5080cd75c
innfi <innfi.world@gmail.com>
2022-11-14 22:42:36 +0900

    linked_container: learning rc

--- linked_container/tests/single_linked_list.rs
+++ linked_container/tests/single_linked_list.rs
@@ -272,7 +272,109 @@ mod tests_reference2 {
     let mut iter = list.iter();
     assert_eq!(iter.next(), Some(&String::from("end")));
 
-    // list is not live and well
+    // list is live and well
     assert_eq!(list.pop(), Some(String::from("end")));
+  }
+}
+
+mod tests_reference3 {
+  use std::rc::Rc;
+
+  pub struct List<T> {
+    head: Link<T>
+  }
+
+  type Link<T> = Option<Rc<Node<T>>>;
+
+  struct Node<T> {
+    elem: T,
+    next: Link<T>
+  }
+
+  impl<T> List<T> {
+    pub fn new() -> Self {
+      List { head: None }
+    }
+
+    pub fn prepend(&self, elem: T) -> List<T> {
+      List {
+        head: Some(Rc::new(
+          Node {
+            elem: elem,
+            next: self.head.clone(),
+          }
+        ))
+      }
+    }
+
+    pub fn tail(&self) -> List<T> {
+      List { head: self.head.as_ref().and_then(|node| node.next.clone()) }
+    }
+
+    pub fn head(&self) -> Option<&T> {
+      self.head.as_ref().map(|node| &node.elem)
+    }
+
+    pub fn iter(&self) -> Iter<'_, T> {
+      Iter { next: self.head.as_deref() }
+    }
+  }
+
+  impl<T> Drop for List<T> {
+    fn drop(&mut self) {
+      let mut head = self.head.take();
+      while let Some(node) = head {
+        if let Ok(mut node) = Rc::try_unwrap(node) {
+          head = node.next.take();
+        } else {
+          break;
+        }
+      }
+    }
+  }
+
+  pub struct Iter<'a, T> {
+    next: Option<&'a Node<T>>,
+  }
+
+  impl<'a, T> Iterator for Iter<'a, T> {
+    type Item = &'a T;
+
+    fn next(&mut self) -> Option<Self::Item> {
+      self.next.map(|node| {
+        self.next = node.next.as_deref();
+
+        &node.elem
+      })
+    }
+  }
+
+  #[test]
+  fn initial_empty_head() {
+    let list = List::<i32>::new();
+
+    assert_eq!(list.head(), None);
+  }
+
+  #[test]
+  fn head_and_tail() {
+    let list = List::new().prepend(1).prepend(2).prepend(3);
+
+    assert_eq!(list.head(), Some(&3));
+
+    let next_list = list.tail();
+    assert_eq!(next_list.head(), Some(&2));
+  }
+
+  #[test]
+  fn iterators() {
+    let list = List::new().prepend(1).prepend(2).prepend(3);
+
+    let mut iter = list.iter();
+
+    assert_eq!(iter.next(), Some(&3));
+    assert_eq!(iter.next(), Some(&2));
+    assert_eq!(iter.next(), Some(&1));
+    assert_eq!(iter.next(), None);
   }
 }
