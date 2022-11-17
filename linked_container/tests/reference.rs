30c48dd24634029e5a79ddb2d2214c9b2662f091
innfi <innfi.world@gmail.com>
2022-11-17 19:33:28 +0900

    linked_container: why do i need Item type?

--- linked_container/tests/reference.rs
+++ linked_container/tests/reference.rs
@@ -340,17 +340,93 @@ mod tests_reference4 {
     }
 
     pub fn push_front(&mut self, elem: T) {
-      let new_instance = Node::new(elem);
+      let new_head = Node::new(elem);
 
       match self.head.take() {
         None => {
-          self.head = Some(new_instance.clone());
-          self.tail = Some(new_instance);
+          self.head = Some(new_head.clone());
+          self.tail = Some(new_head);
         },
         Some(old_head) => {
-
+          old_head.borrow_mut().prev = Some(new_head.clone());
+          new_head.borrow_mut().next = Some(old_head);
+          self.head = Some(new_head);
         }
       }
+    }
+
+    pub fn push_back(&mut self, elem: T) {
+      let new_tail = Node::new(elem);
+
+      match self.tail.take() {
+        Some(old_tail) => {
+          old_tail.borrow_mut().next = Some(new_tail.clone());
+          new_tail.borrow_mut().prev = Some(old_tail);
+          self.tail = Some(new_tail);
+        },
+        None => {
+          self.head = Some(new_tail.clone());
+          self.tail = Some(new_tail);
+        }
+      }
+    }
+
+    pub fn pop_front(&mut self) -> Option<T> {
+      self.head.take().map(|old_head| {
+        match old_head.borrow_mut().next.take() {
+          Some(new_head) => {
+            new_head.borrow_mut().prev.take();
+            self.head = Some(new_head);
+          },
+          None => {
+            self.tail.take();
+          },
+        }
+
+        Rc::try_unwrap(old_head).ok().unwrap().into_inner().elem
+      })
+    }
+
+    pub fn pop_back(&mut self) -> Option<T> {
+      self.tail.take().map(|old_tail| {
+        match old_tail.borrow_mut().prev.take() {
+          Some(new_tail) => {
+            new_tail.borrow_mut().next.take();
+            self.tail = Some(new_tail);
+          },
+          None => {
+            self.tail.take();
+          }
+        }
+
+        Rc::try_unwrap(old_tail).ok().unwrap().into_inner().elem
+      })
+    }
+
+    pub fn peek_front(&self) -> Option<Ref<T>> {
+      self.head.as_ref().map(|node| {
+        Ref::map(node.borrow(), |node| &node.elem)
+      })
+    }
+
+    pub fn into_iter(self) -> IntoIter<T> {
+      IntoIter(self)
+    }
+  }
+
+  pub struct IntoIter<T>(List<T>);
+
+  impl<T> Iterator for IntoIter<T> {
+    type Item = T;
+
+    fn next(&mut self) -> Option<T> {
+      self.0.pop_front()
+    }
+  }
+
+  impl<T> DoubleEndedIterator for IntoIter<T> {
+    fn next_back(&mut self) -> Option<T> {
+      self.0.pop_back()
     }
   }
 }
