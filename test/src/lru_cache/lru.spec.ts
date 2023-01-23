6487bcee0e0a65a2ea6908ba3d8fd9fed01adcf0
innfi <innfi.world@gmail.com>
2023-01-23 21:15:58 +0900

    test: lru cache limit node length

--- test/src/lru_cache/lru.spec.ts
+++ test/src/lru_cache/lru.spec.ts
@@ -8,9 +8,9 @@ describe('lru cache', () => {
 		instance.insert('bbbb');
 		instance.insert('cccc');
 
-		expect(instance.root.data).toBe('aaaa');
+		expect(instance.root.data).toBe('cccc');
 		expect(instance.root.next.data).toBe('bbbb');
-		expect(instance.root.next.next.data).toBe('cccc');
+		expect(instance.root.next.next.data).toBe('aaaa');
 	});
 
 	it('get data', () => {
@@ -28,9 +28,9 @@ describe('lru cache', () => {
 		const instance = new LRUCache();
 
 		const rootData = 'aaaa';
-		instance.insert(rootData);
 		instance.insert('bbbb');
 		instance.insert('cccc');
+		instance.insert(rootData);
 
 		expect(instance.root.data).toBe(rootData);
 
@@ -41,7 +41,25 @@ describe('lru cache', () => {
 		expect(instance.root.next.next.data).toBe('bbbb');
 	});
 
+	it('cache len', () => {
+		const instance = new LRUCache();
+
+		instance.insert('aaaa');
+		instance.insert('bbbb');
+		expect(instance.nodeLen()).toBe(2);
+
+		instance.insert('ccc');
+		expect(instance.nodeLen()).toBe(3);
+	});
+
 	it('inserting data more than limit deletes tail', () => {
-		
+		const instance = new LRUCache(3);
+
+		instance.insert('aaaa');
+		instance.insert('bbbb');
+		instance.insert('cccc');
+		instance.insert('dddd');
+
+		expect(instance.root.next.next.next).toBeUndefined();
 	});
 });
--- test/src/lru_cache/lru_cache.ts
+++ test/src/lru_cache/lru_cache.ts
@@ -3,9 +3,37 @@ import { DoubleLink, DoubleNode } from "../link/double_link";
 export class LRUCache extends DoubleLink {
 	readonly limit: number;
 
-	constructor(limit?: number) {
+	constructor(limit: number = 10) {
 		super();
 		this.limit = limit;
+	}
+
+	insert(data: string): void {
+		if (!this.root) {
+			super.insert(data);
+			return;
+		}
+
+		const temp = this.root;
+
+		this.root = new DoubleNode(data);
+		this.root.next = temp;
+		temp.prev = this.root;
+
+		if (this.nodeLen() > this.limit) this.deleteTail();
+	}
+
+	private deleteTail(): void {
+		let current = this.root;
+
+		while(current) {
+			if (!current.next) break;
+
+			current = current.next;
+		}
+
+	  if (current.prev) current.prev.next = undefined;
+		current = undefined;
 	}
 
 	get(data: string): string {
@@ -34,10 +62,18 @@ export class LRUCache extends DoubleLink {
 	private moveToRoot(data: string): void {
 		this.deleteNode(data);
 
-		const temp = this.root;
+		this.insert(data);
+	}
 
-		this.root = new DoubleNode(data);
-		this.root.next = temp;
-		temp.prev = this.root;
+	nodeLen(): number {
+		let current = this.root;
+		let len = 0;
+
+		while(current) {
+			len += 1;
+			current = current.next;
+		}
+
+		return len;
 	}
 }
