8cacdbedc1bd33362b23e20668d2f87d3e5d8b3d
innfi <innfi.world@gmail.com>
2023-03-22 20:30:28 +0900

    tree-rb: had to use star for struct mutation

--- tree-rb/main.go
+++ tree-rb/main.go
@@ -2,6 +2,36 @@ package main
 
 import "fmt"
 
+type Node struct {
+	nodeValue int
+	next      *Node
+}
+
+type DoubleNode struct {
+	nodeValue int
+	prev      *DoubleNode
+	next      *DoubleNode
+}
+
+type RBTree struct {
+	root *DoubleNode
+}
+
+func (tree *RBTree) insert(nodeValue int) {
+	fmt.Printf("RBTree.insert] \n")
+
+	if tree.root == nil {
+		fmt.Printf("RBTree.insert] empty root \n")
+
+		tree.root = &DoubleNode{
+			nodeValue: nodeValue,
+			prev:      nil,
+			next:      nil,
+		}
+	}
+	fmt.Printf("RBTree.insert] root.nodeValue: %d\n", tree.root.nodeValue)
+}
+
 func main() {
 	fmt.Printf("start from here")
 }
--- tree-rb/tree_test.go
+++ tree-rb/tree_test.go
@@ -9,7 +9,6 @@ import (
 /*
 TOOD
 --------------------------------------------------------------------------------
-- create node and link
 - create tree
 - insert initial node
 - insert nodes
@@ -17,11 +16,26 @@ TOOD
 
 DONE
 --------------------------------------------------------------------------------
+- create node and link
 */
 
-type Node struct {
-	nodeValue int
-	next      *Node
+func TestLink(t *testing.T) {
+	rootNode := DoubleNode{
+		nodeValue: 1,
+		prev:      nil,
+		next:      nil,
+	}
+
+	secondNode := DoubleNode{
+		nodeValue: 2,
+		prev:      &rootNode,
+		next:      nil,
+	}
+
+	rootNode.next = &secondNode
+
+	assert.Equal(t, rootNode.next.nodeValue, 2)
+	assert.Equal(t, secondNode.prev.nodeValue, 1)
 }
 
 func TestInitNode(t *testing.T) {
@@ -32,4 +46,23 @@ func TestInitNode(t *testing.T) {
 
 	assert.Equal(t, instance.nodeValue, 1)
 	assert.Equal(t, instance.next == nil, true)
+}
+
+func TestStateChange(t *testing.T) {
+	tree := RBTree{}
+	tree.root = &DoubleNode{
+		nodeValue: 3,
+		prev:      nil,
+		next:      nil,
+	}
+
+	assert.Equal(t, tree.root.nodeValue, 3)
+}
+
+func TestInsertFirst(t *testing.T) {
+	tree := RBTree{}
+
+	tree.insert(1)
+
+	assert.Equal(t, tree.root.nodeValue, 1)
 }
