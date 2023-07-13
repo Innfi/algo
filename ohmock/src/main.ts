46b996109f51ca20d03a45c2e604b9567c952428
innfi <innfi.world@gmail.com>
2023-07-13 23:04:06 +0900

    ohmock: checking conditions from last pos

--- ohmock/src/main.ts
+++ ohmock/src/main.ts
@@ -1,5 +1,68 @@
 
+export type Fields = string[][];
+export interface Position2D {
+  x: number;
+  y: number;
+}
 
-export class Runner {
+export interface FieldStatus {
+  fields: Fields;
+  lastStonePosition?: Position2D;
+}
 
+export const createNewFields = (ySize: number, xSize: number): string[][] => {
+  const result: string[][] = [];
+  for (let i = 0; i < ySize; i++) {
+    const yArray: Array<"" | "O" | "X"> = [];
+    for (let j = 0; j < xSize; j++) {
+      yArray.push("");
+    }
+    result.push(yArray);
+  }
+  return result;
+};
+
+export interface OmPlayer {
+  getDescription(): { nickname: string; tactics: string; };
+
+  dropTheStone(
+    fieldStatus: FieldStatus,
+    yourFlag: "O" | "X",
+  ): Promise<Position2D>;
+}
+
+export class Runner implements OmPlayer {
+  getDescription(): { nickname: string; tactics: string; } {
+    return {
+      nickname: 'innfi',
+      tactics: 'observing'
+    };
+  }
+
+  async dropTheStone(
+    fieldStatus: FieldStatus,
+    yourFlag: "O" | "X",
+  ): Promise<Position2D> {
+    return {
+      x: 1,
+      y: 1,
+    };
+  }
+
+  findConditionHorizontal(fieldStatus: FieldStatus, targetFlag: 'O'|'X'): Position2D | undefined {
+    const fields = fieldStatus.fields;
+    const lastPos = fieldStatus.lastStonePosition!;
+
+    let posX = lastPos.x;
+    while (posX > 0 && fields[posX][lastPos.y] === targetFlag) {
+      posX--;
+    }
+
+    if (posX < 0) return undefined;
+
+    return {
+      x: posX,
+      y: lastPos.y,
+    };
+  }
 }
--- ohmock/test/test_runner.ts
+++ ohmock/test/test_runner.ts
@@ -1,7 +1,32 @@
 import assert from 'assert';
 
+import { FieldStatus, Runner, createNewFields } from '../src/main';
+
 describe('ohmock', () => {
   it('initial', () => {
     assert.strictEqual(1, 1);
+  });
+
+  it('check condition from last position', () => {
+    const fields = createNewFields(19, 19);
+
+    fields[7][9] = 'O';
+    fields[8][9] = 'O';
+    fields[9][9] = 'O';
+
+    const fieldStatus: FieldStatus = {
+      fields,
+      lastStonePosition: {
+        x: 9,
+        y: 9,
+      },
+    };
+
+    const runner = new Runner();
+    const result = runner.findConditionHorizontal(fieldStatus, 'O');
+
+    assert.strictEqual(result !== undefined, true);
+    assert.strictEqual(result!.x, 6);
+    assert.strictEqual(result!.y, 9);
   });
 });
