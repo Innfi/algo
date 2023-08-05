a6ffe9b57da37a132def265194ed63027807d539
innfi <innfi.world@gmail.com>
2023-08-05 22:31:07 +0900

    skiplist-ts: testing for 1 level

--- skiplist-ts/test/runner_new.ts
+++ skiplist-ts/test/runner_new.ts
@@ -3,37 +3,10 @@ import assert from 'assert';
 import { SkipList } from '../src/skiplist_new';
 
 describe('skiplist_new', () => {
-  // it('basic insert / display', () => {
-  //   const instance = new SkipList();
-
-  //   instance.insert(3);
-  //   instance.insert(6);
-  //   instance.insert(7);
-  //   instance.insert(9);
-  //   instance.insert(12);
-  //   instance.insert(19);
-  //   instance.insert(17);
-  //   instance.insert(26);
-  //   instance.insert(21);
-  //   instance.insert(29);
-  //   instance.insert(28);
-  //   instance.insert(22);
-  //   instance.insert(23);
-  //   instance.insert(24);
-  //   instance.insert(35);
-  //   instance.insert(55);
-
-  //   assert.strictEqual(instance.root.elem, -1);
-
-  //   instance.display();
-  //   instance.deleteOne(21);
-  //   instance.display();
-  // });
-
   it('span] initial state ', () => {
     const instance = new SkipList();
 
-    assert.strictEqual(instance.rootSpan.every((v) => v === 0), true);
+    assert.strictEqual(instance.root.span.every((v) => v === 0), true);
   });
 
   it('span] add a node and its span is one', () => {
@@ -42,7 +15,7 @@ describe('skiplist_new', () => {
     instance.insert(1);
     instance.insert(2);
 
-    assert.strictEqual(instance.rootSpan[0], 1);
+    assert.strictEqual(instance.root.span[0], 1);
     assert.strictEqual(instance.root.forward[0]?.span, 1);
     assert.strictEqual(instance.root.forward[0]?.forward[0]?.span, 0);
   });
@@ -62,6 +35,6 @@ describe('skiplist_new', () => {
     instance.display();
     // console.log(`forward[1]: ${instance.root.forward[1]?.span}`);
 
-    assert.strictEqual(instance.root.forward[1]!.span > 1, true);
+    assert.strictEqual(instance.root.forward[1]!.span[1] > 1, true);
   });
 });
