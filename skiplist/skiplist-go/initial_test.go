47538d3f8ef5b6acd3ffd87f6263b8ad7ca81e75
innfi <innfi.world@gmail.com>
2023-08-14 22:13:52 +0900

    skiplist-go: todo random / init array with zeros

--- skiplist/skiplist-go/initial_test.go
+++ skiplist/skiplist-go/initial_test.go
@@ -21,6 +21,28 @@ func (node *Node) InitNode() {
 	node.span = make([]int, node.levelMax)
 }
 
+func NewNode() *Node {
+	newNode := Node{}
+	newNode.InitNode()
+
+	return &newNode
+}
+
+type SkipList struct {
+	root  *Node
+	level int
+}
+
+func (skipList *SkipList) Init() {
+	skipList.root = NewNode()
+	skipList.level = 0
+}
+
+func (skipList *SkipList) Insert(elem int) {
+	// current := skipList.root
+	// update := make([]*Node, skipList.root.levelMax)
+}
+
 func TestNode(t *testing.T) {
 	node := Node{}
 	node.InitNode()
@@ -28,4 +50,14 @@ func TestNode(t *testing.T) {
 	assert.Equal(t, node.elem, -1)
 	assert.Equal(t, len(node.forward), node.levelMax)
 	assert.Equal(t, len(node.span), node.levelMax)
+}
+
+func TestSkipListInitial(t *testing.T) {
+	instance := SkipList{}
+	instance.Init()
+
+	root := instance.root
+
+	assert.Equal(t, root.elem, -1)
+	assert.Equal(t, len(root.forward), root.levelMax)
 }
