a0e64ddfcc4480cef633c093d77215392856aafb
innfi <innfi.world@gmail.com>
2023-08-21 20:10:33 +0900

    skiplist-go: implement insert

--- skiplist/skiplist-go/initial_test.go
+++ skiplist/skiplist-go/initial_test.go
@@ -9,23 +9,25 @@ import (
 	"github.com/stretchr/testify/assert"
 )
 
+const MAX_LEVEL int = 8
+const LEVEL_PROB float32 = 0.25
+
 type Node struct {
-	elem     int
-	forward  []*Node
-	span     []int
-	levelMax int
+	elem    int
+	forward []*Node
+	span    []int
 }
 
 func (node *Node) InitNode() {
-	node.levelMax = 4
-
 	node.elem = -1
-	node.forward = make([]*Node, node.levelMax)
-	node.span = make([]int, node.levelMax)
+	node.forward = make([]*Node, MAX_LEVEL)
+	node.span = make([]int, MAX_LEVEL)
 }
 
-func NewNode() *Node {
-	newNode := Node{}
+func NewNode(elem int) *Node {
+	newNode := Node{
+		elem: elem,
+	}
 	newNode.InitNode()
 
 	return &newNode
@@ -37,13 +39,81 @@ type SkipList struct {
 }
 
 func (skipList *SkipList) Init() {
-	skipList.root = NewNode()
+	skipList.root = NewNode(-1)
 	skipList.level = 0
+
+	rand.Seed(time.Now().UnixNano())
 }
 
 func (skipList *SkipList) Insert(elem int) {
-	// current := skipList.root
-	// update := make([]*Node, skipList.root.levelMax)
+	current := skipList.root
+	update := make([]*Node, MAX_LEVEL)
+
+	rLevel := skipList.randomLevel()
+	newNode := NewNode(elem)
+
+	for i := skipList.level; i >= 0; i-- {
+		spanSum := 0
+		for current.forward[i] != nil && current.forward[i].elem < elem {
+			spanSum += current.span[i]
+			current = current.forward[i]
+		}
+
+		update[i] = current
+
+		if i < MAX_LEVEL-1 {
+			newNode.span[i+1] = spanSum
+		}
+	}
+
+	newNode.span[0] = 1
+	current = current.forward[0]
+
+	if current != nil && current.elem == elem {
+		return
+	}
+
+	if rLevel > skipList.level {
+		for i := skipList.level + 1; i < rLevel+1; i++ {
+			update[i] = skipList.root
+		}
+
+		skipList.level = rLevel
+	}
+
+	spanSum := 0
+	for i := 0; i <= rLevel; i++ {
+		currentUpdate := update[i]
+
+		newNode.forward[i] = update[i].forward[i]
+		update[i].forward[i] = newNode
+
+		oldSpan := currentUpdate.span[i]
+		newNodeSpan := newNode.span[i]
+
+		spanSum += newNodeSpan
+
+		currentUpdate.span[i] = spanSum
+		newSpan := oldSpan - spanSum
+
+		if newSpan < 0 {
+			newNode.span[i] = 0
+		} else {
+			newNode.span[i] = newSpan
+		}
+	}
+}
+
+func (skipList *SkipList) randomLevel() int {
+	r := rand.Float32()
+	level := 0
+
+	for r < LEVEL_PROB && level < MAX_LEVEL {
+		level++
+		r = rand.Float32()
+	}
+
+	return level
 }
 
 func TestNode(t *testing.T) {
@@ -51,8 +121,8 @@ func TestNode(t *testing.T) {
 	node.InitNode()
 
 	assert.Equal(t, node.elem, -1)
-	assert.Equal(t, len(node.forward), node.levelMax)
-	assert.Equal(t, len(node.span), node.levelMax)
+	assert.Equal(t, len(node.forward), MAX_LEVEL)
+	assert.Equal(t, len(node.span), MAX_LEVEL)
 }
 
 func TestSkipListInitial(t *testing.T) {
@@ -62,7 +132,7 @@ func TestSkipListInitial(t *testing.T) {
 	root := instance.root
 
 	assert.Equal(t, root.elem, -1)
-	assert.Equal(t, len(root.forward), root.levelMax)
+	assert.Equal(t, len(root.forward), MAX_LEVEL)
 }
 
 func TestRandom(t *testing.T) {
@@ -74,4 +144,11 @@ func TestRandom(t *testing.T) {
 		assert.Equal(t, result >= 0, true)
 		assert.Equal(t, result < 8, true)
 	}
+}
+
+func TestMakeArray(t *testing.T) {
+	lists := make([]*Node, 8)
+
+	assert.Equal(t, len(lists), 8)
+	assert.Equal(t, lists[0] == nil, true)
 }
