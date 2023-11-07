fcc124e172d0f73e75b4263cd247c01b4f880c84
innfi <innfi.world@gmail.com>
2023-11-07 23:09:53 +0900

    leaky_bucket: which actor controls the time?

--- leaky_bucket/README.md
+++ leaky_bucket/README.md
@@ -4,3 +4,12 @@
   - bursting traffic refined to a consistent rate traffic
   - fixed size queue, fixed retrival count
   - parameters: bucket size, tick ratio, retrival count
+
+# TODO
+
+* implentations
+  - accept traffic under the physical limit
+  - retrieve traffic as a fixed rate
+
+# DONE
+
--- leaky_bucket/src/main.rs
+++ leaky_bucket/src/main.rs
@@ -1,3 +1,61 @@
+use std::sync::Arc;
+
+use concurrent_queue::ConcurrentQueue;
+use serde::{Serialize, Deserialize};
+
+#[derive(Serialize, Deserialize, Debug)]
+pub struct Payload {
+  msg: String,
+}
+
+pub struct DestinationGateway {
+  // TODO
+}
+
+impl DestinationGateway {
+  pub fn new() -> Self {
+    Self {}
+  }
+
+  pub fn send(&self, _payload: Payload) -> Result<(), &'static str> {
+    // TODO
+
+    Ok(())
+  }
+}
+
+pub struct LeakyBucket {
+  queue: ConcurrentQueue<Payload>,
+  gateway: Arc<DestinationGateway>,
+}
+
+impl LeakyBucket {
+  pub fn new(
+      queue_len: usize,
+      gw: Arc<DestinationGateway>,
+    ) -> Self {
+    Self {
+      queue: ConcurrentQueue::bounded(queue_len),
+      gateway: gw.clone(),
+    }
+  }
+
+  pub fn push(&self, payload: Payload) -> Result<(), &'static str> {
+    if self.queue.is_full() {
+      // what is the decision?
+      return Ok(())
+    }
+
+    self.queue.push(payload).unwrap();
+
+    Ok(())
+  }
+
+  pub fn handle_to_be_renamed(&self) {
+    
+  }
+}
+
 fn main() {
-    println!("Hello, world!");
+  println!("Hello, world!");
 }
