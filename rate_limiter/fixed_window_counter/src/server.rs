baea8a5db896901f29eb39d56749bc58eafc3fa3
innfi <innfi.world@gmail.com>
2023-11-22 21:58:34 +0900

    fixed_window_counter: need timer, atomic variable and mutex

--- rate_limiter/fixed_window_counter/src/server.rs
+++ rate_limiter/fixed_window_counter/src/server.rs
@@ -1,3 +1,45 @@
+use log;
+
+const COUNTER_MAX: usize = 10;
+
+pub struct Request {}
+
+pub struct Handler {
+  counter: usize,
+}
+
+impl Handler {
+  pub fn new() -> Self {
+    Self { counter: 0 }
+  }
+
+  pub fn run(&mut self) {
+    loop {
+      if self.counter >= COUNTER_MAX {
+        log::debug!("threshold. continue");
+        continue;
+      }
+
+      let req = self.receive_request().unwrap();
+
+      self.increment_counter();
+      let _ = self.relay_request(req);
+    }
+  }
+
+  pub fn receive_request(&self) -> Result<Request, &'static str> {
+    Ok(Request{}) //FIXME
+  }
+
+  fn increment_counter(&mut self) {
+    self.counter += 1; //FIXME
+  }
+
+  fn relay_request(&self, _req: Request) -> Result<(), &'static str> {
+
+    Ok(())
+  }
+}
 
 
 #[tokio::main]
