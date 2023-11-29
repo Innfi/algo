b824c571aa61feb263b93cb7663e313821f6393b
innfi <innfi.world@gmail.com>
2023-11-29 23:14:18 +0900

    fixed_window_counter: implementing with actix-web

--- rate_limiter/fixed_window_counter/src/server.rs
+++ rate_limiter/fixed_window_counter/src/server.rs
@@ -1,13 +1,14 @@
+use actix_web::{web, App, HttpResponse, HttpServer};
 use core::time;
 use std::{sync::{Mutex, Arc}, thread};
 use log;
 
-const COUNTER_MAX: usize = 10;
+const COUNTER_MAX: u32 = 10;
 
 pub struct Request {}
 
 pub struct Handler {
-  counter: usize,
+  counter: u32,
 }
 
 impl Handler {
@@ -54,25 +55,51 @@ async fn main() -> Result<(), Box<dyn std::error::Error>> {
     counter_checker(counter_clone).await
   });
 
-  loop {
-    thread::sleep(time::Duration::from_millis(1000));
+  // loop {
+  //   thread::sleep(time::Duration::from_millis(1000));
 
-    let mut counter_mut = counter.lock().unwrap();
-    *counter_mut += 1;
+  //   let mut counter_mut = counter.lock().unwrap();
+  //   *counter_mut += 1;
 
-    if *counter_mut > 20 {
-      break;
-    }
-  }
+  //   if *counter_mut > 20 {
+  //     break;
+  //   }
+  // }
+
+  let _ = HttpServer::new(move || {
+    App::new()
+      .route("/", web::get().to(dummy_couter_gen))
+      .app_data(counter.clone())
+  })
+  .bind("localhost:8080")?
+  .run()
+  .await;
 
   Ok(())
+}
+
+async fn dummy_couter_gen(
+  //counter: Arc<Mutex<u32>>
+  counter: web::Data<Mutex<u32>>,
+) -> HttpResponse {
+  let mut counter_lock = counter.try_lock().unwrap();
+
+  println!("dummy_counter_gen] {}", *counter_lock);
+  if *counter_lock >= 10 {
+    return HttpResponse::TooManyRequests().into();
+  }
+
+  *counter_lock += 1;
+
+  HttpResponse::Ok().into()
 }
 
 async fn counter_checker(counter: Arc<Mutex<u32>>) {
   loop {
     thread::sleep(time::Duration::from_millis(5000));
 
-    let lock_guard = counter.lock().unwrap();
-    println!("counter: {}", *lock_guard);
+    let mut lock_guard = counter.lock().unwrap();
+    *lock_guard = 0;
+    println!("reset")
   }
 }
