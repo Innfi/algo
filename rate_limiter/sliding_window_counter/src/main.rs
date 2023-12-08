d2cd54bede119f2812cb5e42308d9311de3669a2
innfi <innfi.world@gmail.com>
2023-12-08 23:51:54 +0900

    sliding_window_counter: before test

--- rate_limiter/sliding_window_counter/src/main.rs
+++ rate_limiter/sliding_window_counter/src/main.rs
@@ -4,7 +4,8 @@ use core::time;
 use std::{sync::Mutex, thread};
 use log;
 
-const COUNTER_MAX: usize = 5;
+const COUNTER_MAX: f64 = 50.0;
+const WINDOW_LEN: usize = 20;
 
 #[derive(Debug)]
 pub struct ReqCountUnit {
@@ -12,25 +13,67 @@ pub struct ReqCountUnit {
   count: usize,
 }
 
+#[derive(Debug)]
+pub struct SlidingWindow {
+  queue: ConcurrentQueue<ReqCountUnit>,
+  sum: usize,
+  current: usize,
+}
+
 struct WindowHandler {
-  mutex_handle: Mutex<ConcurrentQueue<ReqCountUnit>>,
+  mutex_handle: Mutex<SlidingWindow>,
 }
 
 impl WindowHandler {
   pub fn new() -> Self {
     Self { 
       mutex_handle: Mutex::new(
-        ConcurrentQueue::bounded(COUNTER_MAX*2),
+        SlidingWindow {
+          queue: ConcurrentQueue::bounded(WINDOW_LEN* 2),
+          sum: 0,
+          current: 0,
+        },
       ),
     }
   }
 
   pub fn try_increment(&self) -> Result<(), &'static str> {
-    todo!();
+    let mut lock = self.mutex_handle.try_lock();
+
+    if let Ok(ref mut mutex) = lock {
+      let average: f64 = ((mutex.sum + mutex.current) / WINDOW_LEN) as f64;
+      if average > COUNTER_MAX {
+        return Err("too_many_request");
+      }
+
+      mutex.current += 1;
+      return Ok(());
+    }
+
+    Err("lock_failed")
   }
 
   pub fn try_reset(&self) -> Result<(), &'static str> {
-    todo!();
+    let mut lock = self.mutex_handle.try_lock();
+
+    if let Ok(ref mut mutex) = lock {
+      mutex.queue.pop().expect("queue.pop() failed");
+      mutex.queue.push(ReqCountUnit {
+        id: 0,
+        count: mutex.current,
+      }).expect("queue.push() failed");
+
+      mutex.sum = mutex.queue
+      .try_iter()
+      .by_ref()
+      .map(|x| x.count)
+      .reduce(|a, b| a+b)
+      .expect("calculate_sum_failed");
+
+      mutex.current = 0;
+    }
+
+    Err("lock_failed")
   }
 }
 
