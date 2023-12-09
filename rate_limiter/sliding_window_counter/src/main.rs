40799d2fc37cc57f8b70ff5d4158561adfccc1ba
innfi <innfi.world@gmail.com>
2023-12-09 21:25:37 +0900

    sliding_window_counter: testing

--- rate_limiter/sliding_window_counter/src/main.rs
+++ rate_limiter/sliding_window_counter/src/main.rs
@@ -81,11 +81,48 @@ impl WindowHandler {
 async fn main() -> std::io::Result<()> {
   env_logger::init_from_env(env_logger::Env::new().default_filter_or("info"));
 
+  let window_handler = web::Data::new(WindowHandler::new());
+  let handler_clone = window_handler.clone();
+
+  tokio::spawn(async move {
+    counter_checker(handler_clone).await
+  });
+
   HttpServer::new(move || {
     App::new()
       .wrap(Logger::new("%a %{User-Agent}i"))
+      .route("/", web::get().to(req_handler))
+      .app_data(window_handler.clone())
   })
   .bind(("127.0.0.1", 8080))?
   .run()
   .await
+}
+
+async fn req_handler(handler: web::Data<WindowHandler>) -> impl Responder {
+  let try_incr_result = handler.try_increment();
+  if try_incr_result.is_ok() {
+    return HttpResponse::Ok().body("hello");
+  }
+
+  let err_msg = try_incr_result.unwrap_err();
+  if err_msg == "too_many_request" {
+    return HttpResponse::TooManyRequests().into();
+  }
+
+  HttpResponse::InternalServerError().into()
+}
+
+async fn counter_checker(handler: web::Data<WindowHandler>) {
+  loop {
+    thread::sleep(time::Duration::from_millis(5000));
+
+    let reset_result = handler.try_reset();
+    if reset_result.is_ok() {
+      log::info!("counter_checker] reset");
+      continue;
+    }
+
+    log::info!("try_reset failed");
+  }
 }
