af41f8926c655082e55f3c695080ffeb253a43ae
innfi <innfi.world@gmail.com>
2024-03-24 22:04:56 +0900

    tree_radix: refactor and wrapup

--- tree_radix/tester.ts
+++ tree_radix/tester.ts
@@ -1,5 +1,22 @@
 import assert from 'assert';
 
+const toPrefix = (input: string, key: string): string => {
+  let common = "";
+  let len = input.length < key.length ? input.length : key.length;
+  for (let i=0;i<len;i++) {
+    if (input[i] !== key[i]) break;
+
+    common += input[i];
+  }
+
+  return common;
+}
+
+const removePrefix = (input: string, prefix: string): string => {
+  return input.substring(prefix.length, input.length);
+};
+
+
 class RadixNode {
   public key: string;
   public nodes: RadixNode[];
@@ -13,6 +30,14 @@ class RadixNode {
     return this.nodes.find((v) => v.key === key);
   }
 
+  findNodeByPrefixMatch(key: string): RadixNode {
+    return this.nodes.find((v) => v.key[0] === key[0]);
+  }
+
+  addNode(newNode: RadixNode): void {
+    this.nodes.push(newNode);
+  }
+
   print(): void {
     this.nodes.forEach((v) => {
       console.log(`${this.key} -> ${v.key}`);
@@ -22,85 +47,60 @@ class RadixNode {
 }
 
 class RadixTree {
-  root: RadixNode[];
+  root: RadixNode;
 
   constructor() {
-    this.root = [];
+    this.root = new RadixNode('');
   }
 
   insert(input: string): void {
-    for (const node of this.root) {
-      const subset = this.findSubset(input, node.key);
-
-      if (subset.length <= 0) continue;
-
-      this.insertRecursive(node, input, subset);
+    const nodeByPrefix: RadixNode = this.root.findNodeByPrefixMatch(input);
+    if (!nodeByPrefix) {
+      this.root.addNode(new RadixNode(input));
       return;
     }
 
-    this.root.push(new RadixNode(input));
+    this.insertRecursive(nodeByPrefix, input);
   }
 
-  private insertRecursive(node: RadixNode, input: string, subset: string): void {
+  private insertRecursive(node: RadixNode, input: string): void {
     // assume node.key.len > subset.len
 
-    const remainFromInput = input.substring(subset.length, input.length);
-    const remainFromKey = node.key.substring(subset.length, node.key.length);
+    const prefix = toPrefix(input, node.key);
+    const remainFromInput = removePrefix(input, prefix);
+    const remainFromKey = removePrefix(node.key, prefix);
 
-    node.key = subset;
+    node.key = prefix;
 
     if (remainFromKey.length > 0) {
-      const intermediate = new RadixNode(remainFromKey);
-      intermediate.nodes = node.nodes;
+      const newChild = new RadixNode(remainFromKey);
+      newChild.nodes = node.nodes;
 
-      node.nodes = [intermediate];
+      node.nodes = [newChild];
       node.nodes.push(new RadixNode(remainFromInput));
 
       return;
     } 
 
-    for (const subnode of node.nodes) {
-      const innerSubset = this.findSubset(remainFromInput, subnode.key);
+    const nodeByPrefix: RadixNode = node.findNodeByPrefixMatch(remainFromInput);
+    if (!nodeByPrefix) return;
 
-      if (innerSubset.length <= 0) continue;
-
-      this.insertRecursive(subnode, remainFromInput, innerSubset);
-      return;
-    }
-  }
-
-  private findSubset(input: string, key: string): string {
-    let common = "";
-    let len = input.length < key.length ? input.length : key.length;
-    for (let i=0;i<len;i++) {
-      if (input[i] !== key[i]) break;
-
-      common += input[i];
-    }
-
-    return common;
+    this.insertRecursive(nodeByPrefix, remainFromInput);
   }
 
   print(): void {
-    this.root.forEach((v) => v.print());
+    this.root.print();
   }
 
   findExactMatch(input: string): boolean {
-    // FIXME: maybe root should be RadixNode?
-    const tempNode = new RadixNode('');
-    tempNode.nodes = this.root;
-
-    return this.findExactMatchRecursive(input, tempNode);
+    return this.findExactMatchRecursive(input, this.root);
   }
 
   private findExactMatchRecursive(input: string, node: RadixNode): boolean {
-    const current = node.nodes.find((v) => {
-      return this.findSubset(input, v.key).length > 0;
-    });
-
+    const current = node.findNodeByPrefixMatch(input);
     if (!current) return false;
 
-    const remain = input.substring(this.findSubset(input, current.key).length, input.length);
+    const remain = removePrefix(input, toPrefix(input, current.key));
     if (remain.length <= 0) return true;
 
     return this.findExactMatchRecursive(remain, current);
@@ -122,7 +122,7 @@ describe('test', () => {
     const input = 'first';
     instance.insert(input);
 
-    assert.strictEqual(instance.root.findIndex((v) => v.key === input) >= 0, true);
+    assert.strictEqual(instance.root.nodes.findIndex((v) => v.key === input) >= 0, true);
   });
 
   it ('first and firm', () => {
@@ -134,7 +134,7 @@ describe('test', () => {
     const remainFirst = 'st';
     const remainSecond = 'm';
 
-    const commonNode = instance.root.find((v) => v.key === subset);
+    const commonNode = instance.root.nodes.find((v) => v.key === subset);
     assert.strictEqual(commonNode.key, subset);
 
     assert.strictEqual(commonNode.findNode(remainFirst) !== undefined, true);
@@ -154,7 +154,7 @@ describe('test', () => {
     const subset2 = 'r';
     const subset3 = 'nal';
 
-    const node1 = instance.root.find((v) => v.key === subset1);
+    const node1 = instance.root.nodes.find((v) => v.key === subset1);
     assert.strictEqual(node1 !== undefined, true);
 
     const node2 = node1.findNode(subset2);
